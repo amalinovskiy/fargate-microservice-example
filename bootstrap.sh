9d7f8d8b7d17153537c60c2baf0021ad69fca655
Anton Malinovskiy <anton@nuroa.com>
2023-01-25 23:39:49 +0100

    Added bootstrap / deploy scripts

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+# Assume role for infrastructure creation
+aws sts assume-role --role-arn "arn:aws:iam::$1:role/OrganizationAccountAccessRole" --role-session-name BoostrapSession > .aws_session
+AWS_ACCESS_KEY_ID=$(cat .aws_session | jq -r .Credentials.AccessKeyId)
+AWS_ACCESS_KEY_SECRET=$(cat .aws_session | jq -r .Credentials.SecretAccessKey)
+AWS_SESSION_TOKEN=$(cat .aws_session | jq -r .Credentials.SessionToken)
+
+cp ~/.aws/credentials ~/.aws/credentials.bak
+printf '[default]
+aws_access_key_id = %s
+aws_secret_access_key = %s
+aws_session_token = %s
+' $AWS_ACCESS_KEY_ID $AWS_ACCESS_KEY_SECRET $AWS_SESSION_TOKEN > ~/.aws/credentials
+
+# Bootstrap CDK
+cdk bootstrap aws://$1/$2
+
+# Bring back original credentials
+cp ~/.aws/credentials.bak ~/.aws/credentials
